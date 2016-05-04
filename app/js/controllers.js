0463a6a8c7053782a9aa167c1b799c8df2f4cd80
Swapnil Patwa <patwaswapnil@gmail.com>
2016-05-05 02:18:30 +0530

    backrop remov

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,9 @@ angular.module('myApp.controllers', [])
     .controller('AppCtrl', ['$scope', function ($scope) { 
     }])
     .controller('LoginCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+    	 $scope.$on("$destroy", function() {
+        $('.modal-backdrop').css('display', 'none'); 
+    });
        $rootScope.title = 'Login';  
     }])
     
